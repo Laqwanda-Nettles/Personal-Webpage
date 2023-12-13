51ec6fecba28fff893691351a144bbce43e5f48b
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2023-12-13 10:52:30 -0600

    created an array, for loop, and function

--- script.js
+++ script.js
@@ -23,3 +23,21 @@ function showQuote() {
     quote.innerHTML = "Huh?";
   }
 }
+
+let output = document.getElementById("output");
+let affirmations = [
+  "Nothing can dim the light that shines from within.",
+  "I am in the right place at the right time, doing the right thing.",
+  "You are loved just for being who you are, just for existing.",
+  "I AM GOOD ENOUGH!",
+  "Your life is about to be incredible.",
+  "Failure is just another way to learn how to do something right.",
+  "Hold your head up, you are deserving of all good things coming to you!",
+];
+
+function getAffirmation() {
+  for (affirmation of affirmations) {
+    output.innerHTML =
+      affirmations[Math.floor(Math.random() * affirmations.length)];
+  }
+}
